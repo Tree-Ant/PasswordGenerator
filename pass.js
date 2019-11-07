d74002be49e3a646106103d52433026a65fe77e7
Tree Ant <stokes.finance@gmail.com>
2019-11-06 20:55:40 -0600

    created initial format for web page

--- pass.js
+++ pass.js
@@ -1,1 +1,16 @@
 console.log("hello");
+
+var char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*"]
+
+var num = char[Math.floor(Math.random() * char.length)];
+
+
+    function print(){
+    for (var i = 0; i < 16; i++) {
+        char[Math.floor(Math.random() * char.length)
+        
+    }
+    }
+
+    print()
+    
