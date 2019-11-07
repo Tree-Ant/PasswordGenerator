fd6115bbae704735bade9225e489d3b0b0b806db
Tree Ant <stokes.finance@gmail.com>
2019-11-06 22:36:58 -0600

    password is now generating and button working

--- pass.js
+++ pass.js
@@ -1,16 +1,19 @@
-console.log("hello");
-
 var char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*"]
 
 var num = char[Math.floor(Math.random() * char.length)];
 
+var s = "";
+for (var i = 0; i < 16; i++) {
+    s += char[Math.floor(Math.random() * char.length)];
+}
+console.log(s);
 
-    function print(){
-    for (var i = 0; i < 16; i++) {
-        char[Math.floor(Math.random() * char.length)
-        
-    }
-    }
 
-    print()
-    
+
+
+    //for (var i = 0; i < 16; i++) {
+      //  document.getElementById("demo") = (char[Math.floor(Math.random() * char.length)]);
+   // }
+
+
+
